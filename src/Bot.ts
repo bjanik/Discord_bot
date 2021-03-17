3d2b8774562fe22e0b157e8a167b10c0dcc1426b
Jordandiazfr <jordandiazfr@gmail.com>
2021-03-17 11:31:02 +0100

    Exported function

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,17 +1,11 @@
 //import { hello_reply } from './hello_reply';
 import Discord from "discord.js";
 import {token} from "../config";
+import ping from "./commands/PingCommand"
 
 const client = new Discord.Client();
 
 const prefix = "!";
-const jojo = (message) => {
-    message.reply("pong")
-}
-const funcs = {
-    //"hello": hello_reply,
-    "ping": jojo,
-}
 
 client.login(token)
 
@@ -23,7 +17,7 @@ client.once("ready", () => {
 client.on('message', msg => {
 // command 1 
     if (msg.content === 'ping') {
-      msg.reply('Pong!');
+        ping(msg)        
     }
 // command 2 
 // command 3  etc.
