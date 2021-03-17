a903d6e9a869e11c3278007a0824fdec97fc8431
Jordandiazfr <jordandiazfr@gmail.com>
2021-03-17 11:49:21 +0100

    base typescript

--- src/Bot.ts
+++ src/Bot.ts
@@ -2,6 +2,7 @@
 import Discord from "discord.js";
 import {token} from "../config";
 import ping from "./commands/PingCommand"
+import lire from "./commands/LireCommand"
 
 const client = new Discord.Client();
 
@@ -16,10 +17,13 @@ client.once("ready", () => {
 
 client.on('message', msg => {
 // command 1 
-    if (msg.content === 'ping') {
+    if (msg.content === '!ping') {
         ping(msg)        
     }
 // command 2 
+if (msg.content === '!lire') {
+    lire(msg)        
+}
 // command 3  etc.
 
   });
