27c5d01fb4f7d119c4a19fcf4093616649de542f
Jordandiazfr <jordandiazfr@gmail.com>
2021-03-18 21:16:13 +0100

    action deploy

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+sudo ansible-playbook deploy.yml -i inv --key-file ~/.ssh/tobkey
---
+++ tests/lire.test.ts
@@ -0,0 +1,8 @@
+import ping from "../src/commands/PingCommand"
+import { assert, expect } from 'chai';
+
+describe('request', () =>{
+    it('Request should be 200', () => {
+        assert.equal()
+    })
+})
