85b8604290c41a7907cf0e539000b8e8f5cca91c
Maxime Chagnon <luffy-star-93@hotmail.fr>
2021-03-18 10:48:13 +0100

    update help message

--- src/commands/HelpCommand.ts
+++ src/commands/HelpCommand.ts
@@ -7,15 +7,20 @@ export default function help(msg){
 
 Tob permet d'écouter votre musique provenant de youtube mais aussi de donner de la lecture provenant de thehackernews.com ou bien de jouer a la roulette russe !
 
+!help //Affiche les commandes du bot 🔍
+
 !play 'youtube link' //Ajoute votre vidéo a la file d'attente et 
-lance la musique dans votre channel vocal
-!play //Relance la musique
-!stop  //Stop la musique
-!skip //Passe a la musique suivante
+lance la musique dans votre channel vocal ⏯️
+!play //Relance la musique ⏯️ 
+!stop  //Stop la musique ⏹️ 
+!skip //Passe a la musique suivante ⏭️ 
             
-!read //Affiche un article aléatoire de thehackernews.com
+!read //Affiche un article aléatoire de thehackernews.com 📖 
             
-!roulette //Jeux de la roulette russe
+!roulette //Jeux de la roulette russe 💥
+
+!8ball 'message' //Boule Magique 🎱 
+
 \`\`\` 
 `
 
