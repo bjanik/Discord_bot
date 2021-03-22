7f12395dd6a4e001161c027dc5f6e11a486f52f3
Bart <bartlomiej.janik@hotmail.fr>
2021-03-22 11:23:32 +0100

    Improved tests for kaamelott and roulette command

--- src/commands/kaamelottCommand.ts
+++ src/commands/kaamelottCommand.ts
@@ -4,21 +4,30 @@ const url: string = 'https://kaamelott.chaudie.re/api/random';
 
 const AxiosInstance = axios.create();
 
-export const create_reply = data => {
+export function create_reply(data): string {
     const sentence = data.citation.citation
     const character = data.citation.infos.personnage
     const reply = `${sentence} - ${character}`
     return reply
 }
 
+export function get_reply(response): string {
+    let reply: string
+    if (response.status === 200) {
+        const data = response.data
+        reply = create_reply(data)
+    }
+    else {
+        reply = `Sorry dude, server returned ${response.status} status code`
+    }
+    return reply
+}
+
 export default function kaamelott(msg) {
     AxiosInstance.get(url)
     .then(response => {
-        if (response.status === 200) {
-            const data = response.data
-            const reply = create_reply(data)
-            msg.reply(reply)
-        }
+        const reply = get_reply(response)
+        msg.reply(reply)
     })
     .catch(console.error)
 }
--- tests/kaamelott.test.ts
+++ tests/kaamelott.test.ts
@@ -1,7 +1,9 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter'
 import { expect } from 'chai';
-import { create_reply } from "../src/commands/kaamelottCommand"
+import { create_reply, get_reply } from "../src/commands/kaamelottCommand"
 
-const data = {
+const citation_data = {
     "citation" : {
         "citation": "C'est pas faux",
         "infos": {
@@ -10,8 +12,36 @@ const data = {
     }
 }
 
+describe("test get_reply", () => {
+    it("should return an error message because request returned a status code != 200", () => {
+        let mock = new MockAdapter(axios)
+        let response = {
+            status: 404,
+            data: citation_data
+        }
+        mock.onGet('https://kaamelott.chaudie.re/api/random').reply(404, citation_data)
+        expect(get_reply(response)).to.equal("Sorry dude, server returned 404 status code")
+
+        response = {
+            status: 500,
+            data: citation_data
+        }
+        mock.onGet('https://kaamelott.chaudie.re/api/random').reply(404, citation_data)
+        expect(get_reply(response)).to.equal("Sorry dude, server returned 500 status code")
+    })
+})
+
 describe("create_reply", () => {
     it("should return sentence - character", () => {
-        expect(create_reply(data)).to.equal("C'est pas faux - Perceval")
+        let mock = new MockAdapter(axios)
+        const response = {
+            status: 200,
+            data: citation_data
+        }
+        mock.onGet('https://kaamelott.chaudie.re/api/random').reply(200, response)
+        expect(create_reply(citation_data)).to.equal("C'est pas faux - Perceval")
+
+        expect(get_reply(response)).to.equal("C'est pas faux - Perceval")
+
     })
 })
